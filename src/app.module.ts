d5b8f512eec036955e919c7cee7dcaec091ee57d
hanhvn <vonhathanh@gmail.com>
2026-03-30 14:49:43 +0700

    some thoughtful comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Environment } from './enum';
 
+// forRoot(): global initialization
+// - configure a module once for the whole app
+// - shared/singleton
+// - usually called in app.module
+// - example: setup db connection, configure external services such as: queue, redis cache
+
+// forFeature(): module-level initialization
+// - use/extend modules initialized by forRoot()
+// - register feature-specific providers
+// - called inside feature modules
+// - example: TypeOrmModule.forFeature([User]) -> create User repository and make it injectable in that module
+
 @Module({
   // Optional list of imported modules that export the providers which are required in this module.
   imports: [
