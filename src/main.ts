51885df3a07528f28e32f307ec2d8c8513e18eba
hanhvn <vonhathanh@gmail.com>
2026-03-30 16:31:34 +0700

    update log

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Environment } from './enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
-  console.log(`Application running at port: ${process.env.PORT}, ${process.env.NODE_ENV} mode`);
+  console.log(
+    `Application running at port: ${process.env.PORT}\nMode: ${process.env.NODE_ENV}\nAuto sync db: ${process.env.NODE_ENV == Environment.DEV}`,
+  );
 }
 bootstrap();
