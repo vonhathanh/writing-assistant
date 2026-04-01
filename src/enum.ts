1b4522f1abd54eff76870b7c27e8d1c435c5ad64
hanhvn <vonhathanh@gmail.com>
2026-04-01 14:28:04 +0700

    add Genre enum

--- src/enum.ts
+++ src/enum.ts
@@ -2,3 +2,14 @@ export const enum Environment {
   DEV = 'development',
   PROD = 'production',
 };
+
+export enum Genre {
+  FANTASY = 'Fantasy',
+  SCIENCE_FICTION = 'Science Fiction',
+  MYSTERY = 'Mystery',
+  THRILLER = 'Thriller',
+  ROMANCE = 'Romance',
+  WESTERN = 'Western',
+  DYSTOPIAN = 'Dystopian',
+  CONTEMPORARY = 'Contemporary',
+}
