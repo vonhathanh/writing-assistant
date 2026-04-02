995daa4e05c3094dfc4c0f9d88da3af7cfc9e3ce
hanhvn <vonhathanh@gmail.com>
2026-04-02 20:57:33 +0700

    make AbstractEntity extends BaselineEntity (only has id & created_at column)

--- src/lib/abstract-class/abtractEntity.ts
+++ src/lib/abstract-class/abtractEntity.ts
@@ -1,12 +1,14 @@
 import { PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
 
-export abstract class AbstractEntity {
+export abstract class BaselineEntity {
   @PrimaryGeneratedColumn('identity')
   id: number;
 
   @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
   createdAt: Date;
+}
 
+export abstract class AbstractEntity extends BaselineEntity {
   @CreateDateColumn({ name: 'updated_at', type: 'timestamptz' })
   updatedAt: Date;
 }
