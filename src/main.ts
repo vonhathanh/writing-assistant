2adc6b7c42c1a242a3f97ceb74f7740e128d1d5a
hanhvn <vonhathanh@gmail.com>
2026-04-28 09:20:29 +0700

    setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { Environment } from './enum';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // UI available at /api
   await app.listen(process.env.PORT);
   console.log(
     `Application running at port: ${process.env.PORT}`,
